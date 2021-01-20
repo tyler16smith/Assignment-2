afb823efd6a784e5c34cadf6f411143530f5e9a0
tyler16smith <56706463+tyler16smith@users.noreply.github.com>
2021-01-20 14:57:00 -0700

    Add project files.

---
+++ wwwroot/js/calcGrade.js
@@ -0,0 +1,32 @@
+﻿
+function calcGrade() {
+    var assignments = document.getElementById("assignments").value * 0.50;
+    var groupprojects = document.getElementById("groupprojects").value * 0.10;
+    var quizzes = document.getElementById("quizzes").value * 0.10;
+    var exams = document.getElementById("exams").value * 0.20;
+    var intex = document.getElementById("intex").value * 0.10;
+
+    var finalGrade = assignments + groupprojects + quizzes + exams + intex;
+    finalGrade = finalGrade.toFixed(2)
+
+    if (finalGrade >= 90) {
+        letterGrade = "A"
+    }
+    else if (finalGrade >= 80) {
+        letterGrade = "B"
+    }
+    else if (finalGrade >= 70) {
+        letterGrade = "C"
+    }
+    else if (finalGrade >= 60) {
+        letterGrade = "D"
+    }
+    else if (finalGrade >= 50) {
+        letterGrade = "F"
+    }
+    else {
+        letterGrade = "E"
+    }
+
+    alert("Final grade: " + finalGrade + "% (" + letterGrade + ")");
+}
